08109f3533e52a5b689c0c6b573554ed7fd34f13
divyanshupathak <divyanshu@deligence.com>
2017-01-13 17:44:45 +0530

    remove extra code

--- copy/imports/ui/App.jsx
+++ copy/imports/ui/App.jsx
@@ -1,23 +1,23 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createContainer } from 'meteor/react-meteor-data';
-
-// import { Tasks } from '../api/Tasks.js';
-
-// import Task from './Task.jsx';
-// import AccountsUIWrapper from './AccountsUIWrapper.jsx';
  
 // App component - represents the whole app
 export default class App extends Component {
+	componentDidMount(){
+		/*var Clipboard = require('clipboard');
+		var clipboard = new Clipboard('.copyButton');
+
+		clipboard.on('success', function(e) {
+			alert("Text copied successfully");
+			e.clearSelection();
+		});
+
+		clipboard.on('error', function(e) {
+			console.error("Error -->", e);
+		});*/
+	}
   copyText(event){
     event.preventDefault();
-
-    var name = this.refs.name.value;
-    var email = this.refs.email.value;
-    var data={
-      "name" : name,
-      "email" : email
-    }
 
     Meteor.call('copyToclipboard', (err, res)=>{
       if(err){
@@ -29,13 +29,18 @@ export default class App extends Component {
           if(res == ""){
             alert('Link not generated');
           }else{
-            console.log(document.queryCommandSupported('copy')); // print true
+            console.log("Copy supported >>>>", document.queryCommandSupported('copy')); // print true
             var field = document.getElementById('content');
             console.log("Invitation url >>>", field.value, "---- field.value.length ----", field.value.length)
             field.focus();
             field.setSelectionRange(0, field.value.length);
             var success = document.execCommand("copy");
             console.log("Copy success >>>>>>>", success)
+            if(success){
+            	$('#log').text('Copy success');
+            }else{
+            	$('#log').text('Copy failed !');
+            }
           }
         }else if(res == false){
           alert('Email ID already exists');
@@ -47,22 +52,14 @@ export default class App extends Component {
     return (
       <div>
         <form className="well">
-          <div className="row">
-            <div className="form-group col-sm-2">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" ref="name" placeholder="First Name" required />
-            </div>
-            <div className="form-group col-sm-2">
-              <label htmlFor="email">Email</label>
-              <input type="email" className="form-control" id="email" ref="email"  placeholder="Email" required />
-            </div>
-            <div className="col-sm-2 submit-button">
-              <div className="form-group">
-                <button type="button" className="btn btn-default form-control" onClick={this.copyText.bind(this)}>Copy text</button>
-              </div>
-            </div>
-            <input type="text" className="form-control CopyText" id="content" />
+        	<h3>Copy To Clipboard</h3>
+          <div className="submit-button">
+          	<div className="form-group">
+          		<button type="button" className="btn btn-default form-control copyButton" onClick={this.copyText.bind(this)}>Copy text</button><br />
+          		<p id="log" style={{"color":"red"}}></p>
+          	</div>
           </div>
+          <input type="text" className="form-control CopyText" id="content" />
         </form>
       </div>
     );
