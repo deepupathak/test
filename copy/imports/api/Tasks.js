21a2b54c910977a416c9813fed2111fc968fcc7c
divyanshupathak <divyanshu@deligence.com>
2017-01-13 17:51:29 +0530

    remove extra code

--- copy/imports/api/Tasks.js
+++ copy/imports/api/Tasks.js
@@ -3,7 +3,6 @@ import { Random } from 'meteor/random';
 
 Meteor.methods({
 	'copyToclipboard': function() {
-
 		var url = '';
 		var token = Random.secret();
 		url = 'https://abc.com/user/'+ token;
