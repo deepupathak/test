c7869d7f16ae95c3217252fb8282758fb9706d1b
divyanshupathak <divyanshu@deligence.com>
2017-01-13 18:19:34 +0530

    remove extra code

--- copy/imports/ui/App.jsx
+++ copy/imports/ui/App.jsx
@@ -16,50 +16,50 @@ export default class App extends Component {
 			console.error("Error -->", e);
 		});*/
 	}
-  copyText(event){
-    event.preventDefault();
+	copyText(event){
+		event.preventDefault();
 
-    Meteor.call('copyToclipboard', (err, res)=>{
-      if(err){
-        alert( err, 'danger', 'growl-top-right');
-      }else{
-        if(res){
-          $('#content').val(res);
-          console.log("----", $('#content').val())
-          if(res == ""){
-            alert('Link not generated');
-          }else{
-            console.log("Copy supported >>>>", document.queryCommandSupported('copy')); // print true
-            var field = document.getElementById('content');
-            console.log("Invitation url >>>", field.value, "---- field.value.length ----", field.value.length)
-            field.focus();
-            field.setSelectionRange(0, field.value.length);
-            var success = document.execCommand("copy");
-            console.log("Copy success >>>>>>>", success)
-            if(success){
-            	$('#log').text('Copy success');
-            }else{
-            	$('#log').text('Copy failed !');
-            }
-          }
-        }
-      }
-    });
-  }
-  render() {
-    return (
-      <div>
-        <form className="well">
-        	<h3>Copy To Clipboard</h3>
-          <div className="submit-button">
-          	<div className="form-group">
-          		<button type="button" className="btn btn-default form-control copyButton" onClick={this.copyText.bind(this)}>Copy text</button><br />
-          		<p id="log" style={{"color":"red"}}></p>
-          	</div>
-          </div>
-          <input type="text" className="form-control CopyText" id="content" />
-        </form>
-      </div>
-    );
-  }
+		Meteor.call('copyToclipboard', (err, res)=>{
+			if(err){
+				alert( err, 'danger', 'growl-top-right');
+			}else{
+				if(res){
+					$('#content').val(res);
+					console.log("----", $('#content').val())
+					if(res == ""){
+						alert('Link not generated');
+					}else{
+						console.log("Copy supported >>>>", document.queryCommandSupported('copy')); // print true
+						var field = document.getElementById('content');
+						console.log("Invitation url >>>", field.value, "---- field.value.length ----", field.value.length)
+						field.focus();
+						field.setSelectionRange(0, field.value.length);
+						var success = document.execCommand("copy");
+						console.log("Copy success >>>>>>>", success)
+						if(success){
+						$('#log').text('Copy success');
+						}else{
+							$('#log').text('Copy failed !');
+						}
+					}
+				}
+			}
+		});
+	}
+	render() {
+		return (
+			<div>
+				<form className="well">
+					<h3>Copy To Clipboard</h3>
+					<div className="submit-button">
+						<div className="form-group">
+							<button type="button" className="btn btn-default form-control copyButton" onClick={this.copyText.bind(this)}>Copy text</button><br />
+							<p id="log" style={{"color":"red"}}></p>
+						</div>
+					</div>
+					<input type="text" className="form-control CopyText" id="content" />
+				</form>
+			</div>
+		);
+	}
 }
